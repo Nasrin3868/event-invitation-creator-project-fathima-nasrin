9f51ad36f7c26600e145a243f74c1f8c2530c4ed
Nasrin3868 <er.fathimanasrin@gmail.com>
2024-11-09 16:59:07 +0530

    Validated form fields by adding a modal

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Function to add event to local storage
+function addEventToLocalStorage(eventName,eventDate,startTime,endTime,description,location){
+    
+    const event={
+        eventName,
+        eventDate,
+        startTime,
+        endTime,
+        description,
+        location
+    }
+    localStorage.setItem("event", JSON.stringify(event))
+}
+
+function handleSubmit(){
+    event.preventDefault()
+    
+    const eventName=document.getElementById("event-name").value
+    const eventDate=document.getElementById("event-date").value
+    const startTime=document.getElementById("start-time").value
+    const endTime=document.getElementById("end-time").value
+    const description=document.getElementById("description").value
+    const location=document.getElementById("location").value
+    if(!eventName||!eventDate||!startTime||!endTime||!description||!location){
+        showError()
+        return
+    }
+    addEventToLocalStorage(eventName,eventDate,startTime,endTime,description,location)
+
+    document.getElementById("event-form").reset()
+    const eventToDisplay=getEventsFromLocalStorage()
+    displayEvent(eventToDisplay)
+}
+// Function to show the modal
+function showError(){
+    document.getElementById("error-modal").style.display = "flex";
+    document.body.style.overflow = "hidden";
+}
+
+// Function to close the modal
+function closeError(){
+    document.getElementById("error-modal").style.display = "none"; 
+    document.body.style.overflow = "auto";
+}
+
+document.getElementById("event-form").addEventListener("submit", handleSubmit)
+document.getElementById("close-modal").addEventListener("click",closeError)
